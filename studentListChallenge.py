69a94e32b34ce072fe80136a6aed004283e7882e
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-12-21 17:28:02 -0500

    Create studentListChallenge.py

---
+++ studentListChallenge.py
@@ -0,0 +1,16 @@
+students = [
+    {"id":1 ,"first_name": "John", "last_name": "Doe", "phone": "123-456-7890"},
+    {"id":2 ,"first_name": "Jane", "last_name": "Smith", "phone": "987-654-3210"},
+    {"id":3 ,"first_name": "Alice", "last_name": "Johnson", "phone": "555-123-4567"},
+    {"id":4 ,"first_name": "Bob", "last_name": "Brown", "phone": "555-987-6543"},
+    {"id":5 ,"first_name": "Carol", "last_name": "Davis", "phone": "555-456-7890"},
+    {"id":6 ,"first_name": "Eve", "last_name": "White", "phone": "555-654-3210"},
+    {"id":7 ,"first_name": "Frank", "last_name": "Green", "phone": "555-321-4567"},
+    {"id":8 ,"first_name": "Grace", "last_name": "Black", "phone": "555-213-6543"},
+    {"id":9 ,"first_name": "Hank", "last_name": "Wilson", "phone": "555-789-0123"},
+    {"id":10 ,"first_name": "Ivy", "last_name": "Moore", "phone": "555-654-0987"}
+]
+
+for student in students:
+    id= str(student["id"])
+    first_name = student["first_name"]
